06437835de844fa321c29f87e55d606b44efba94
hpjaj <hmlevine@gmail.com>
2015-05-16 10:14:57 -0600

    Refactored dictionary_sequencer.rb

--- dictionary_sequencer.rb
+++ dictionary_sequencer.rb
@@ -1,7 +1,7 @@
 class DictionarySequencer
 
   def initialize
-    @arr_of_arr = []
+    @sequence_pairings = []
   end
 
   def sequence_dictionary(text_file)
@@ -11,40 +11,31 @@ class DictionarySequencer
       range_of_indeces = word_length - 4
       if word_length >= 4
         (0..range_of_indeces).each do |index|
-          piece = word.slice(index, 4)
-          build_array_of_pairs(word, piece)
+          sequence = word.slice(index, 4)
+          build_array_of_pairs(word, sequence)
         end
       end
       puts word  # this is to watch the progress in the terminal
     end
 
-    output_sequences
-    output_words
+    output_results('sequences.txt', 0)
+    output_results('words.txt', 1)
   end
 
   private
 
-  def build_array_of_pairs(word, piece)
-    if @arr_of_arr.any? { |pair| pair[0] == piece }
-      duplicate_pairs_index = @arr_of_arr.find_index { |pair| pair[0] == piece }
-      @arr_of_arr.delete_at(duplicate_pairs_index)
+  def build_array_of_pairs(word, sequence)
+    if ( duplicate_pairs_index = @sequence_pairings.find_index { |pair| pair[0] == sequence } )
+      @sequence_pairings.delete_at(duplicate_pairs_index)
     else
-      @arr_of_arr << [piece, word]
+      @sequence_pairings << [sequence, word]
     end
   end
 
-  def output_sequences
-    File.open('sequences.txt', 'w') do |file|
-      @arr_of_arr.each do |pair|
-        file.puts pair[0]
-      end
-    end
-  end
-
-  def output_words
-    File.open('words.txt', 'w') do |file|
-      @arr_of_arr.each do |pair|
-        file.puts pair[1]
+  def output_results(file, index)
+    File.open(file, 'w') do |file|
+      @sequence_pairings.each do |pair|
+        file.puts pair[index]
       end
     end
   end
